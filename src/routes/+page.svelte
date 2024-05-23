43edb04ac68a91e82517c4a3df5bfdcf68535647
olaleykhan <realjoy9@gmail.com>
2024-05-23 16:20:04 +0100

    bare metal svelte kit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+
+    // The scripts for the index page go here
+
+    console.log("jello")
+
+    let name = "Olalekan Abdulfatah"
+</script>
+
+<!-- then markup for the index page goes here -->
+
+<h1> Hello and welcome to the page created by {name}</h1>
+
+
+
+
+<!-- style goes below here -->
+
+<style>
+    h1 {
+        color: red;
+    }
+</style>
