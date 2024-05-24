fa991ab4b864d0b8f04596802c9f9d52680a1c6f
olaleykhan <realjoy9@gmail.com>
2024-05-24 04:53:08 +0100

    delete item

--- src/lib/components/TodoItem.svelte
+++ src/lib/components/TodoItem.svelte
@@ -1,13 +1,22 @@
-<script>
+<script lang="ts" >
     export let todo;
-    // export let dispatch;
+  import { createEventDispatcher, afterUpdate } from 'svelte';
+
+  
+  const dispatch = createEventDispatcher();
+
+  function handleDelete(id:string){
+    //   console.log("I am being deleted.... id: ", id)
+      dispatch('delete', {id})
+    }
+
+    afterUpdate(() => {
+        console.log("I am being updated.... awnnn")
+    })
 </script>
 
 <li>
     <input type="checkbox" bind:checked={todo.completed}>
     <span>{todo.text}</span>
-    <button on:click={() => {
-        // dispatch('delete', todo.id)
-        console.log("I am being clicked to delete todo.... awnnn")
-    }}>{todo.completed? "Delete": "Remove"}</button>
+    <button on:click={() => handleDelete(todo.id) }>{todo.completed? "Delete": "Remove"}</button>
 </li>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,12 @@
-<script>
+<script lang="ts" >
 
     // The scripts for the index page go here
     import {Button, TodoItem} from "$lib"
+	import type { H } from "vitest/dist/reporters-yx5ZTtEV.js";
 
     let count = 0;
     let name = "Olalekan Abdulfatah"
-    function handleClick(){
-        console.log("I am being clicked.... awnnn")
-        if(count>0){
-            // this is actually not going to work at all since the onclick method has the once modifier
-            alert("I am sorry, I can't work anymore. I am tired.")
-            return;
-        }
-        count++;
-        console.log("I work normally as I am humanly expected to do.")
-
-        
-    }
-    function handleClick2(){
-        console.log("eskelebe ti o lebebe")
-    }
-
+    let todoInput = "";
     let todos = [
         {
             id: 1,
@@ -38,6 +24,51 @@
             done: false
         }
     ]
+    function handleClick(){
+        console.log("I am being clicked.... awnnn")
+        if(count>0){
+            // this is actually not going to work at all since the onclick method has the once modifier
+            alert("I am sorry, I can't work anymore. I am tired.")
+            return;
+        }
+        count++;
+        console.log("I work normally as I am humanly expected to do.")
+ 
+    }
+    
+    const handleOnChange = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    todoInput = target.value;
+  };
+
+  function handleDelete(event: Event){
+      console.log("I am being deleted.... id: ", event)
+    //   TODO: terrible typescript support. improve typing support in project
+      const id = event.detail.id as number ;
+      todos = todos.filter(todo => todo.id !== id)
+    }
+
+//     function handleRemoveTodo(id) {
+//     dispatch('removetodo', {
+//       id
+//     });
+//   }
+
+
+    function handleSumbit(){
+        console.log("I am being submitted.... awnnn")
+
+        console.log("todos are: ",todos)
+        const todo = {
+            id: todos.length + 1,
+            text: todoInput,
+            done: false
+        }
+        todos = [...todos, todo];
+        todoInput = "";
+    }
+
+
 </script>
 
 <!-- then markup for the index page goes here -->
@@ -47,9 +78,14 @@
         <p> This is the index page of the SvelteKit app.</p>
         <ul>
             {#each todos as todo (todo.id)}
-                <TodoItem {todo} />
+                <TodoItem {todo} on:delete={handleDelete} />
             {/each}
         </ul>
+
+        <form on:submit|preventDefault={handleSumbit}>
+            <input type="text" on:input={handleOnChange} placeholder="Enter your todo" bind:value={todoInput} />
+            <Button >Click me</Button>
+        </form>
     </main>
 
 
